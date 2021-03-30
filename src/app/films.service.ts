bbbb8c041837e8152387153a8e905ec2d35830a6
brunodantascg <brunodedantas@gmail.com>
2021-03-30 14:26:48 -0300

    create InMemoryData

---
+++ src/app/films.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Films } from './films';
+import { FILMS } from './mock-films';
+
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FilmsService {
+
+  private filmsUrl = 'api/films'; 
+
+  constructor(
+    private http: HttpClient) { }
+
+  getFilms(): Observable <Films[]> {
+    return this.http.get<Films[]>(this.filmsUrl)
+      .pipe(
+
+      );
+  }
+}
---
+++ src/app/films.ts
@@ -0,0 +1,11 @@
+export interface Films {
+    id: number;
+    nome: string;
+    ano: number;
+    genero: string;
+    classificacao: string;
+    duracao: string;
+    idioma: string;
+    produtora: string;
+    pais: string
+}
